0bf1b124d4a66f98f7f6fd8f3815cb0df3acf75e
InsaneFury <justdarsan@gmail.com>
2019-08-20 09:29:16 -0300

    Prototype with OpenGL and GLFW

---
+++ Engine/Engine/BaseGame.h
@@ -0,0 +1,14 @@
+#ifndef BASE_GAME
+#define BASE_GAME
+
+#define DLLEXPORT __declspec(dllexport)
+
+class DLLEXPORT BaseGame
+{
+public:
+	 BaseGame();
+	 int Window();
+};
+
+#endif //BASE_GAME
+
---
+++ Engine/Game/Game.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BaseGame.h"
+
+using namespace std;
+
+void main() {
+	BaseGame game;
+	game.Window();
+	cin.get();
+}
